830ad8ea6c3aecbef1b2370e002f4c1f1635d305
Michael Wolfe <mikewolfe.490@gmail.com>
2018-03-13 16:50:02 -0400

    add documentation and clean up code

--- find_motifs.py
+++ find_motifs.py
@@ -7,20 +7,43 @@ import scipy.optimize as opt
 np.random.seed(1234)
 
 def make_initial_seeds(cats, wsize,wstart,wend):
+    """ Function to make all possible seeds, superceded by the precompute
+    all windows method in seq_database
+    """
     seeds = []
     for param in cats:
         for window in param.sliding_windows(wsize, start=wstart, end=wend):
             seeds.append(window)
     return seeds
 
-def optimize_mi(param_vec, data):
+def optimize_mi(param_vec, data, sample_perc):
+    """ Function to optimize a particular motif
+
+    Args:
+        param_vec (list) - values to optimize. Last value is threshold
+        data (SeqDatabase) - full seq database
+        sample_perc(float) - percentage of database to calculate MI over
+    Returns:
+        MI for the parameters over a random sample of the seq database
+        as determined by 
+    """
     threshold = param_vec[-1]
-    this_data = data.random_subset(args.optimize_perc)
+    this_data = data.random_subset(sample_perc)
     this_discrete = generate_peak_vector(this_data, param_vec[:-1], threshold)
     this_mi = this_data.mutual_information(this_discrete)
     return this_mi
 
 def generate_peak_vector(data, motif_vec, threshold):
+    """ Function to calculate the sequences that have a match to a motif
+    Args:
+        data (SeqDatabase) - database to calculate over, must already have
+                             motifs pre_computed
+        motif_vec(np.array) - numpy array containing motif vector
+        threshold(float) - a distance threshold to be considered a match
+    Returns:
+        discrete (np.array) - a numpy array of 1 or 0 for each sequence where
+                              1 is a match and 0 is not.
+    """
     this_discrete = []
     for this_seq in data.iterate_through_precompute():
         seq_pass = 0
@@ -30,13 +53,22 @@ def generate_peak_vector(data, motif_vec, threshold):
                 seq_pass = 1
                 break
         this_discrete.append(seq_pass)
-    return this_discrete
+    return np.array(this_discrete)
 
-def find_initial_threshold(cats, wsize, wstart, wend):
+def find_initial_threshold(cats):
+    """ Function to determine a reasonable starting threshold given a sample
+    of the data
+
+    Args:
+        cats (SeqDatabase) - database to calculate over, must already have
+                             motifs pre_computed
+    Returns:
+        threshold (float) - a threshold that is the
+                            mean(distance)-2*stdev(distance)
+    """
     seeds = []
-    for param in cats:
-        for window in param.sliding_windows(wsize, start=wstart, end=wend):
-            seeds.append(window)
+    for this_seq in cats.iterate_through_precompute():
+        seeds.extend(this_seq)
     distances = []
     for seed1 in seeds:
         for seed2 in seeds:
@@ -50,29 +82,66 @@ def find_initial_threshold(cats, wsize, wstart, wend):
     return np.exp(mean-2*stdev)
 
 def seqs_per_bin(cats):
+    """ Function to determine how many sequences are in each category
+
+    Args:
+        cats (SeqDatabase) - database to calculate over
+    Returns:
+        outstring - a string enumerating the number of seqs in each category
+    """
     string = ""
     for value in np.unique(cats.values):
-        string += "Cat %i: %i\n"%(value, np.sum(np.array(cats.values) ==  value))
+        string += "\nCat %i: %i"%(value, np.sum(np.array(cats.values) ==  value))
     return string
 
 def two_way_to_log_odds(two_way):
+    """ Function to determine the log odds from a two way table
+
+    Args:
+        two_way (list)- a list of 0-cat1Truecat2True 1-cat1Falsecat2True 
+                                  2-cat1Truecat2False 3-cat1Falsecat2False
+    Returns:
+        outstring - a string enumerating the number of seqs in each category
+    """
     num = np.array(two_way[0], dtype=float)/np.array(two_way[1],dtype=float)
     denom = np.array(two_way[2], dtype=float)/np.array(two_way[3],dtype=float)
     return np.log(num/denom)
 
 def read_parameter_file(infile):
+    """ Wrapper to read a single parameter file
+
+    Args:
+        infile (str) - input file name
+    Returns:
+        inout.FastaFile object containing data
+    """
     fastadata = inout.FastaFile()
     with open(infile) as f:
         fastadata.read_whole_datafile(f)
     return fastadata
 
 class MotifMatch(Exception):
+    """ Exception class used for greedy search. To be raised when a motif
+    match is found
+    """
     def __init__(self, value):
         self.value = value
     def __str__(self):
         return "Distance is %f"%self.value
 
 def greedy_search(cats, threshold = 10, number=1000):
+    """ Function to find initial seeds by a greedy search
+
+    Prints the number of seeds per class to the logger
+
+    Args:
+        cats (inout.SeqDatabase) - input data, must have motifs already 
+                                   precomputed
+        threshold (float) - threshold for considering a motif a match
+        number (int) - number of seeds to stop near 
+    Returns:
+        seeds (list) - a list of dsp.ShapeParamSeq objects to be considered
+    """
     seeds = []
     values = []
     cats_shuffled = cats.shuffle()
@@ -121,13 +190,17 @@ if __name__ == "__main__":
     logging.warning("Reading in files")
     all_params = [read_parameter_file(x) for x in args.params]
     cats = inout.SeqDatabase(names=[])
-    cats.read(args.infile,float)
     if args.continuous is not None:
+        cats.read(args.infile, float)
         logging.warning("Discretizing data")
         logging.warning(cats.values[0:10])
         cats.discretize_quant(args.continuous)
         logging.warning(cats.values[0:10])
+    else:
+        cats.read(args.infile, int)
+    logging.warning("Distribution of sequences per class:")
     logging.warning(seqs_per_bin(cats))
+
     for name, param in zip(cats.names, cats):
         for this_param, this_param_name in zip(all_params, args.param_names):
             param.add_shape_param(dsp.ShapeParamSeq(this_param_name, this_param.pull_entry(name).seq))
@@ -135,51 +208,50 @@ if __name__ == "__main__":
     logging.warning("Normalizing parameters")
     cats.normalize_params()
 
-    logging.warning("Determining inital threshold")
-    threshold = find_initial_threshold(cats.random_subset(args.threshold_perc), args.windowsize, wstart=args.windowstart, wend=args.windowend)
-    logging.warning("Using %f as an initial threshold"%(threshold))
-
-    #all_15mers = make_initial_seeds(cats.category_subset(1), 15, 2, 498)
-
     logging.warning("Precomputing all windows")
     cats.pre_compute_windows(args.windowsize, wstart=args.windowstart, wend=args.windowend)
+
+    logging.warning("Determining inital threshold")
+    threshold = find_initial_threshold(cats.random_subset(args.threshold_perc))
+    logging.warning("Using %f as an initial threshold"%(threshold))
+
     all_mi = []
-    #threshold = 10
+
+    #for motif1 in [possible_motifs[x] for x in np.random.randint(0,high=len(possible_motifs), size=100)]:
     logging.warning("Greedy search for possible motifs")
     possible_motifs = greedy_search(cats, 2*threshold, args.num_seeds)
     logging.warning("%s possible motifs"%(len(possible_motifs)))
-    logging.warning("Finding seeds")
-#    for motif1 in [possible_motifs[x] for x in np.random.randint(0,high=len(possible_motifs), size=100)]:
+    logging.warning("Finding MI for seeds")
     for i,motif1 in enumerate(possible_motifs):
         if not (i % 10):
-            logging.warning("Computing motif %s"%i)
+            logging.warning("Computing MI for motif %s"%i)
         this_mi = 0
         this_discrete = []
-        if args.optimize_perc != 1:
+        if args.seed_perc != 1:
             this_cats = cats.random_subset(args.seed_perc)
         else:
             this_cats = cats
         this_discrete = generate_peak_vector(this_cats, motif1.as_vector(cache=True), threshold)
         this_mi = this_cats.mutual_information(this_discrete)
         this_entropy = this_cats.shannon_entropy(this_discrete)
-        all_mi.append((this_mi, this_entropy, motif1.as_vector(cache=True)))
+        all_mi.append((this_mi, this_entropy, motif1))
     logging.warning("Sorting seeds")
     all_mi = sorted(all_mi, key=lambda x: x[0])
     logging.warning(all_mi[-1:])
-    motif_to_optimize = list(all_mi[-1][-1])
+    motif_to_optimize = list(all_mi[-1][-1].as_vector(True))
     logging.warning("Calculating Enrichment for top 10 seeds")
     for motif in all_mi[-10:]:
-        logging.warning(motif)
-        enriched = cats.calculate_enrichment(np.array(generate_peak_vector(cats, motif[-1], threshold)))
+        logging.warning(motif[-1].as_vector(True))
+        enriched = cats.calculate_enrichment(np.array(generate_peak_vector(cats, motif[-1].as_vector(True), threshold)))
         for key in sorted(enriched.keys()):
             logging.warning("Enrichment for Cat %s is %s"%(key, two_way_to_log_odds(enriched[key])))
 
     logging.warning("Taking top seed to optimize")
     logging.warning(motif_to_optimize)
     motif_to_optimize.append(threshold)
-    logging.warning(optimize_mi(motif_to_optimize, cats))
+    logging.warning(optimize_mi(motif_to_optimize, cats, sample_perc = args.optimize_perc))
     logging.warning("Optimizing top seed using Nelder Mead")
-    final = opt.minimize(lambda x: -optimize_mi(x, data=cats), motif_to_optimize, method="nelder-mead", options={'disp':True})
+    final = opt.minimize(lambda x: -optimize_mi(x, data=cats, sample_perc=args.optimize_perc), motif_to_optimize, method="nelder-mead", options={'disp':True})
     logging.warning(final['x'])
     #final = opt.basinhopping(lambda x: -optimize_mi(x, data=cats), motif_to_optimize)
     #logging.warning(final)
