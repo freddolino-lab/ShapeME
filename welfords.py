1e52f42b24fc7ce43f41079d7be8f027be279ab4
Michael Wolfe <mikewolfe.490@gmail.com>
2018-08-15 10:07:05 -0400

    add test for welford's algorithm

--- welfords.py
+++ welfords.py
@@ -19,3 +19,20 @@ class Welford(object):
     def final_stdev(self):
         return math.sqrt(self.sqdist/(self.tot-1.0))
 
+if __name__ == "__main__":
+    import numpy as np
+    data = np.random.normal(30, 10, 10000)
+    online_mean = Welford()
+    for val in data:
+        online_mean.update(val)
+    welford_mean = online_mean.final_mean()
+    welford_stdev = online_mean.final_stdev()
+    actual_mean = np.mean(data)
+    actual_stdev = np.std(data, ddof=1)
+    print "Mean"
+    print "Welfords: %s Actual: %s"%(welford_mean, actual_mean)
+    print "Stdev"
+    print "Welfords: %s Actual: %s"%(welford_stdev, actual_stdev)
+
+
+
